c07829b05850f561bd634298e14c906f76e4cf23
nicerobot <git@nicerobot.org>
2015-10-31 00:26:27 -0400

    tick. gil.

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ GOPATH=$(PWD)
 PY3LIB=$(shell pkg-config --cflags --libs python3)
 
 test: pygo.so
-	python3 -c 'import pygo, sys; print(pygo.sum(40, 2)); sys.stdin.read(1);'
+	python3 -c 'import pygo, sys; print(pygo.sum(40, 2)); pygo.gil(); pygo.tick(); sys.stdin.read(1);'
 
 pygo.so: py.go
 	@go build -v -buildmode=c-shared -o pygo.so
--- pyx.c
+++ pyx.c
@@ -2,6 +2,8 @@
 #include <Python.h>
 
 PyObject * sum(PyObject *, PyObject *);
+PyObject * tick(PyObject *, PyObject *);
+PyObject * gil(PyObject *, PyObject *);
 
 // Workaround missing variadic function support
 // https://github.com/golang/go/issues/975
@@ -11,6 +13,8 @@ int PyArg_ParseTuple_LL(PyObject * args, long long * a, long long * b) {
 
 static PyMethodDef PyGoMethods[] = {
 	{"sum", sum, METH_VARARGS, "Add two numbers."},
+	{"tick", tick, METH_VARARGS, "Start ticker."},
+	{"gil", gil, METH_VARARGS, "Test GIL."},
 	{NULL, NULL, 0, NULL}
 };
 
